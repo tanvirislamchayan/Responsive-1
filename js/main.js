70bef3e84474ea4fc4433edf65c52e554699df31
tanvirislamchayan <imdtanvir181@gmail.com>
2024-09-03 18:28:49 +0600

    added buy now and product details page

--- js/main.js
+++ js/main.js
@@ -93,5 +93,65 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
         header.classList.toggle('sticky', window.scrollY > 200);
     });
+
+    const shopToggler = document.getElementById('filter-toggler');
+    const shopFilter = document.getElementById('shop-filter');
+    
+    shopToggler.addEventListener('click', ()=> {
+        if (shopFilter.classList.contains('show')) {
+            shopFilter.classList.remove('show');
+        } else {
+            shopFilter.classList.add('show')
+        }
+    });
+
+    
 });
 
+// quantity calculation
+var input = document.querySelector('#qty');
+var btnminus = document.querySelector('.qtyminus');
+var btnplus = document.querySelector('.qtyplus');
+
+if (input !== undefined && btnminus !== undefined && btnplus !== undefined && input !== null && btnminus !== null && btnplus !== null) {
+	
+	var min = Number(input.getAttribute('min'));
+	var max = Number(input.getAttribute('max'));
+	var step = Number(input.getAttribute('step'));
+
+	function qtyminus(e) {
+		var current = Number(input.value);
+		var newval = (current - step);
+		if(newval < min) {
+			newval = min;
+		} else if(newval > max) {
+			newval = max;
+		} 
+		input.value = Number(newval);
+		e.preventDefault();
+	}
+
+	function qtyplus(e) {
+		var current = Number(input.value);
+		var newval = (current + step);
+		if(newval > max) newval = max;
+		input.value = Number(newval);
+		e.preventDefault();
+	}
+		
+	btnminus.addEventListener('click', qtyminus);
+	btnplus.addEventListener('click', qtyplus);
+  
+} // End if test
+
+// img toggle
+const mainImg = document.querySelector('.main-img img');
+const subImgs = document.querySelectorAll('.sub-img img')
+
+subImgs.forEach(img => {
+    img.addEventListener('click', ()=> {
+        let tmp = mainImg.src;
+        mainImg.src = img.src;
+        img.src = tmp;
+    });
+});
